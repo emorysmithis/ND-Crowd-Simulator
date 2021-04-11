d362111e6339b91dd2345fd1c6aed816e2167996
Emory Smith <emorysmithis@gmail.com>
2021-04-11 15:58:53 -0400

    getting list of buildings names

--- osm.py
+++ osm.py
@@ -1,8 +1,18 @@
+#!/usr/bin/env python
+
 import osmnx as ox 
 import matplotlib.pyplot as plt
+import pandas as p 
 
 place_name = 'Notre Dame, Indiana, United States'
-graph = ox.graph_from_place(place_name)
-fig, ax = ox.plot_graph(graph)
-plot = plt.tight_layout() 
-plt.show(plot)
+buildings = ox.geometries_from_place(place_name, tags={'building':True})
+bnf = 'building_names.xlsx'
+buildings_set = set()
+for name in buildings['name']:
+    if type(name) == str:
+        print(name)
+        buildings_set.add(name)
+buildings_list = list(buildings_set)
+buildings_list.sort()
+df = p.DataFrame(buildings_list)
+df.to_excel(bnf)
