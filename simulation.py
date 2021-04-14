98d884f837ef222b48f0049549386a3446bc3a6a
Christina Youn <cyoun@ndedu>
2021-04-13 21:39:54 -0400

    simulation with crowding_dict

--- simulation.py
+++ simulation.py
@@ -4,19 +4,19 @@ import os
 import json
 from datetime import time, timedelta, datetime, date
 
-def load_students():
+def load_students(students_file):
     table = []          # master list of students
 
-    # Get list of json file names
-    json_files = [json_file for json_file in os.listdir('./') if json_file.endswith('.json')]
-
-    # Load student data into table
-    for json_file in json_files:
-        with open(json_file) as data:
-            student = json.load(data)
-            table.append(student)
+    with open(students_file) as data:
+        table = json.load(data)
 
     return table
+
+def add_edgeid(D, edge_id):
+    if edge_id not in D:            # if edge_id not in dictionary
+        D[edge_id] = 0
+    D[edge_id] += 1                 # increment count
+    return D
 
 def remove_files():
     json_files = [json_file for json_file in os.listdir('./') if json_file.endswith('.json')]
@@ -28,14 +28,16 @@ def remove_files():
 if __name__ == '__main__':
 
     # Initialize parameters
-    table = load_students()
+    students_file = 'test.json'             # TODO: commandline argument
     curr_time = time(7, 45, 0)              # TODO: determine start time -> 8 am
     end_time = time(12, 5, 0)               # TODO: determine end time -> 8 pm
+    N = 3                                   # TODO: set N accordingly
     crowding_dict = {}
+    table = load_students(students_file)
 
     # Main loop
     while curr_time != end_time:
-        print(curr_time)
+        curr_dict = {}
 
         # Iterate through each student
         for student in table:
@@ -47,11 +49,13 @@ if __name__ == '__main__':
                 # Student should continue walking along edge
                 if edge_index < student['journey'][0]['segments'][0]['edge_length']:
                     student['edge_index'] += student['speed']
+                    add_edgeid(curr_dict, student['journey'][0]['segments'][0]['edge_id'])
                     print(curr_time, student['id'], student['journey'][0]['segments'][0]['path_index'], student['edge_index'])
                 # Student should change to a new edge
                 elif len(student['journey'][0]['segments']) > 1:
                     student['journey'][0]['segments'].pop(0)
                     student['edge_index'] = student['speed']
+                    add_edgeid(curr_dict, student['journey'][0]['segments'][0]['edge_id'])
                     print(curr_time, student['id'], student['journey'][0]['segments'][0]['path_index'], student['edge_index'])
                 # Student has arrived at destination
                 else:
@@ -73,11 +77,25 @@ if __name__ == '__main__':
                 # Student should start moving
                 if (datetime.combine(date(1,1,1),class_time) - timedelta(minutes=2, seconds=estimated_time)).time() < curr_time:
                     student['edge_index'] = student['speed']
+                    add_edgeid(curr_dict, student['journey'][0]['segments'][0]['edge_id'])
                     print(curr_time, student['id'], student['journey'][0]['segments'][0]['path_index'], student['edge_index'])
-            
+        
+        # Update crowding_dict
+        for edge_id, value in curr_dict.items():
+            if edge_id not in crowding_dict:
+                crowding_dict[edge_id] = value
+            elif value > crowding_dict[edge_id]:
+                crowding_dict[edge_id] = value
+    
         # Update curr_time
         curr_time = (datetime.combine(date(1,1,1),curr_time) + timedelta(seconds=1)).time()
-        
+    
+    # Print N most crowded edges 
+    crowding_dict = {k: v for k, v in sorted(crowding_dict.items(), key=lambda item: item[1], reverse=True)}
+    for i, (key, value) in enumerate(crowding_dict.items()):
+        if i < N:
+            print(key, value)
+            
 
     # Remove files
     #remove_files()             # TODO: uncomment later
