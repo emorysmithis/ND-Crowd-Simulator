6ab77e46a3d6af7992fcad3849e29947348f706c
Emory Smith <emorysmithis@gmail.com>
2021-04-27 23:39:19 -0400

    finished statement

--- paths_automate.py
+++ paths_automate.py
@@ -8,13 +8,14 @@ def worker(input_list):
     command = './paths_simulation.py ' + ' '.join(input_list)
     print(command)
     os.system(command)
-    print('finished')
+    print(f'Finished: {command}')
 
 if __name__ == '__main__':
     
     # Initialize variables
     directories = sys.argv[1:]
     days = ['m', 't', 'w', 'r', 'f']
+    #days = ['m'] #TODO: for testing 
     processes = []
     start = ''
     end = ''
@@ -30,14 +31,14 @@ if __name__ == '__main__':
         # Run simulation for each day of the week
         for day in days:
             input_file = d + '/' + day + '_students.txt'
-            print(input_file) 
+            #print(input_file) 
             # Run simulation for each combination of paths 
             step = 10
-            for fp1 in range(30, 110, 10): 
+            for fp1 in range(30, 110, 10): # TODO: changed 110 to 50  
                 upper_limit = 100 - fp1 + 10
-                for fp2 in range(0, upper_limit, 10):
+                for fp2 in range(0, upper_limit, 10): # TODO: changed upper_limit to 20 
                     fp_arg = str(fp1) + '_' + str(fp2)
-                    print(fp_arg) 
+                    #print(fp_arg) 
             
                     output_file = d + '/output_' + day + '.txtTEST'
                     process = mp.Process(target=worker, args=(['-s', input_file, '-start', start, '-end', end, '-n', '50', '-fp', fp_arg, '>', output_file],))
